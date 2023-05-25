6d51430b1dfd17c32b2ea085b44168bcd6e640f0
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-05-25 15:39:54 +0600

    added destroy method to article; added comment with create and destroy method; added model visible; added status for article and comment;

--- db/migrate/20230524145159_add_status_to_articles.rb
+++ db/migrate/20230524145159_add_status_to_articles.rb
@@ -1,5 +1,5 @@
 class AddStatusToArticles < ActiveRecord::Migration[6.0]
   def change
-    add_column :articles, :status, :string
+    add_column :articles, :status, :string, default: "public"
   end
 end
